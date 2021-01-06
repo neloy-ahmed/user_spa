b7cbf6a18386e398b4e292b6a287b88b3c78f930
Neloy Ahmed <web_one@pulseservicesbd.com>
2021-01-06 20:03:56 +0600

    before go

---
+++ app/Http/Controllers/OneController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Resources\UserResource;
+
+
+class OneController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:60',
+            'email' => 'email|required|unique:users',
+            'password' => 'required|confirmed'
+        ]);
+
+        $validatedData['password'] = bcrypt($request->password);
+
+        $user = User::create($validatedData);
+
+        $accessToken = $user->createToken('authToken')->accessToken;
+
+        return response([ 'user' => $user, 'access_token' => $accessToken]);
+    }
+
+    public function login(Request $request)
+    {
+        $loginData = $request->validate([
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);
+
+        if (!auth()->attempt($loginData)) {
+            return response(['message' => 'Invalid Credentials']);
+        }
+
+        $accessToken = auth()->user()->createToken('authToken')->accessToken;
+
+        return response(['user' => auth()->user(), 'access_token' => $accessToken]);
+
+    }
+
+    public function logout (Request $request) {
+        // dd($request);
+        $token = $request->user()->token();
+        $token->revoke();
+        $response = ['message' => 'You have been successfully logged out!'];
+        return response($response, 200);
+    }
+
+    public function update(Request $request, User $user)
+    {
+        
+        $user->update($request->all());
+
+        return response([ 'user' => new UserResource($user), 'message' => 'Updated successfully'], 200);
+    }
+
+}
---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OneController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+
+
+Route::post('/register', 'App\Http\Controllers\OneController@register');
+Route::post('/login', 'App\Http\Controllers\OneController@login');
+Route::post('/logout', 'App\Http\Controllers\OneController@logout')->middleware('auth:api');
+
+Route::patch('/users/{user}', 'App\Http\Controllers\OneController@update')->middleware('auth:api');
